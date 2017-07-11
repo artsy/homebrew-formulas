0b444f2f646ff36de6cca714f6af8f3ff47fa5a6
Jon Allured <jon.allured@gmail.com>
2017-07-11 13:56:40 -0500

    Bump version for deploy_pr

--- Formula/deploy_pr.rb
+++ Formula/deploy_pr.rb
@@ -1,8 +1,8 @@
 class DeployPr < Formula
   desc "Automate deploy pr creation"
   homepage "https://github.com/jonallured/deploy_pr"
-  url "https://github.com/jonallured/deploy_pr/archive/v0.0.5.tar.gz"
-  sha256 "888f136d6b7c3d69a9fc6a1896291488928060b489042c8ba684d0c3a017e3c1"
+  url "https://github.com/jonallured/deploy_pr/archive/v0.0.6.tar.gz"
+  sha256 "221cbad7984750ef3f449c1118600313945f2210bfa34ffdc8844a33eba7b338"
 
   def install
     bin.install 'deploy_pr'
