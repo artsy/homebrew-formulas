7c45e0da03e5daafb20c6fc8afd178db1ea68453
Jon Allured <jon.allured@gmail.com>
2018-07-26 11:44:17 -0500

    Bump the version of copy_env to support hokusai

--- Formula/copy_env.rb
+++ Formula/copy_env.rb
@@ -1,8 +1,8 @@
 class CopyEnv < Formula
-  desc "Automate dotenv setup with values from Heroku"
-  homepage "https://github.com/jonallured/copy_env"
-  url "https://github.com/jonallured/copy_env/archive/v0.0.1.tar.gz"
-  sha256 "089de16845a5a38ae93ba9427e331598b3794fc92e3ccfcd434cd33bc3b6fa3b"
+  desc 'Automate dotenv setup with values from Heroku'
+  homepage 'https://github.com/jonallured/copy_env'
+  url 'https://github.com/jonallured/copy_env/archive/v0.0.2.tar.gz'
+  sha256 '7983c2a53c89dd6cf0b5e390a8d3c1f6d05ce97817ced9a6fc24db58ce184b5c'
 
   def install
     bin.install 'copy_env'
