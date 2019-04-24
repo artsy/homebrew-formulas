9b5b90197900f477b66451fc416001359e1478ea
Isac Petruzzi <isac.petruzzi@gmail.com>
2019-04-24 18:57:17 +0200

    correct hokusai sha256

--- Formula/hokusai.rb
+++ Formula/hokusai.rb
@@ -2,7 +2,7 @@ class Hokusai < Formula
   desc 'Hokusai is a Docker + Kubernetes CLI for application developers'
   homepage 'https://github.com/artsy/hokusai'
   url 'https://artsy-provisioning-public.s3.amazonaws.com/hokusai/homebrew/hokusai-0.5.4.tar.gz'
-  sha256 'ba3ba75343f33fc8f3dd5544d843e25b0ab868061d5caa62e61e0331d1056f08'
+  sha256 '446fbb32ff41aca43619f9092eca9fa0626ae4e8007a4ba5cb473f2e75825ca3'
   version '0.5.4'
 
   def install
