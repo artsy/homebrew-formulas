55420d1691c02944fc7f21c60d9e78fda22c3883
Jon Allured <jon.allured@gmail.com>
2019-07-30 12:21:43 -0500

    Comply with RuboCop

--- Formula/deploy_pr.rb
+++ Formula/deploy_pr.rb
@@ -1,8 +1,8 @@
 class DeployPr < Formula
-  desc "Automate deploy pr creation"
-  homepage "https://github.com/jonallured/deploy_pr"
-  url "https://github.com/jonallured/deploy_pr/archive/v0.0.6.tar.gz"
-  sha256 "221cbad7984750ef3f449c1118600313945f2210bfa34ffdc8844a33eba7b338"
+  desc 'Automate deploy pr creation'
+  homepage 'https://github.com/jonallured/deploy_pr'
+  url 'https://github.com/jonallured/deploy_pr/archive/v0.0.6.tar.gz'
+  sha256 '221cbad7984750ef3f449c1118600313945f2210bfa34ffdc8844a33eba7b338'
 
   def install
     bin.install 'deploy_pr'
