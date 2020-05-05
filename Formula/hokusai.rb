d432b29ffccc4d7a44e92b2abc70880f4e7c1997
Isac Petruzzi <isac.petruzzi@gmail.com>
2020-05-05 13:23:37 +0200

    correct hokusai checksum

--- Formula/hokusai.rb
+++ Formula/hokusai.rb
@@ -2,7 +2,7 @@ class Hokusai < Formula
   desc 'Hokusai is a Docker + Kubernetes CLI for application developers'
   homepage 'https://github.com/artsy/hokusai'
   url 'https://artsy-provisioning-public.s3.amazonaws.com/hokusai/hokusai-0.5.9-Darwin-x86_64.tar.gz'
-  sha256 'a41097f1f48169ca8f43d622e0844debcc428ef951c1ccbd53c60783704a026b'
+  sha256 '5aba5065fd0c26d1fa215b850519fafbbd39109c24212a3126099e0d16fd6092'
   version '0.5.9'
 
   def install
