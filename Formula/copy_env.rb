5d8ce61ceebfda0818bcfb95bea7f01a8eefb3dc
Jon Allured <jon.allured@gmail.com>
2020-10-09 13:09:06 -0500

    Bump copy_env

--- Formula/copy_env.rb
+++ Formula/copy_env.rb
@@ -1,8 +1,8 @@
 class CopyEnv < Formula
-  desc 'Automate dotenv setup with values from Heroku'
+  desc 'Automate dotenv setup with values from Hokusai and Heroku'
   homepage 'https://github.com/jonallured/copy_env'
-  url 'https://github.com/jonallured/copy_env/archive/v0.0.2.tar.gz'
-  sha256 '7983c2a53c89dd6cf0b5e390a8d3c1f6d05ce97817ced9a6fc24db58ce184b5c'
+  url 'https://github.com/jonallured/copy_env/archive/v0.0.3.tar.gz'
+  sha256 '356d2a469fa2bbac56cc955ca7a4317ee2beeabe36cc5b015e966547cf71951f'
 
   def install
     bin.install 'copy_env'
