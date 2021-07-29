853b15b1f47e080cb92a4ef6a42007e26fb42788
Hokusai CI <it@artsymail.com>
2021-07-29 17:40:55 +0000

    Release Hokusai 0.5.15

--- Formula/hokusai.rb
+++ Formula/hokusai.rb
@@ -1,9 +1,9 @@
 class Hokusai < Formula
   desc 'Hokusai is a Docker + Kubernetes CLI for application developers'
   homepage 'https://github.com/artsy/hokusai'
-  url 'https://artsy-provisioning-public.s3.amazonaws.com/hokusai/hokusai-0.5.14-Darwin-x86_64.tar.gz'
-  sha256 '8f9c469e20d69c1e967468fdba534ed55551981ebd63cce261bc5ad6b95c53a3'
-  version '0.5.14'
+  url 'https://artsy-provisioning-public.s3.amazonaws.com/hokusai/hokusai-0.5.15-Darwin-x86_64.tar.gz'
+  sha256 'a0d742a4f0311a31346045bbd3e75fae83df33357a4583e79e498eb1bd65c334'
+  version '0.5.15'
 
   def install
     bin.install 'hokusai'
