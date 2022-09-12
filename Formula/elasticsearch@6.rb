582dab57f89ecda6b67f8dd8705534ad934879e7
Joey Aghion <joey@aghion.com>
2022-09-12 17:53:51 -0400

    Add custom elasticsearch@6 formula for openjdk@17 fix

---
+++ Formula/elasticsearch@6.rb
@@ -0,0 +1,157 @@
+class ElasticsearchAT6 < Formula
+    desc "Distributed search & analytics engine"
+    homepage "https://www.elastic.co/products/elasticsearch"
+    url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-6.8.23.tar.gz"
+    sha256 "60e77b5ca3ce11771469bcc2e009c49c8aadb831faebd170e7abcedc16b3e36d"
+    license "Apache-2.0"
+  
+    bottle do
+      sha256 cellar: :any_skip_relocation, arm64_monterey: "4db242272e294706f8cf26a6d4d33b8c68a9e47db10d7d8ca84d2a3139a39311"
+      sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4db242272e294706f8cf26a6d4d33b8c68a9e47db10d7d8ca84d2a3139a39311"
+      sha256 cellar: :any_skip_relocation, monterey:       "3d77a4b086fa69e59b3f33b344131868588bcb558035b862961e514a0c3273e4"
+      sha256 cellar: :any_skip_relocation, big_sur:        "3d77a4b086fa69e59b3f33b344131868588bcb558035b862961e514a0c3273e4"
+      sha256 cellar: :any_skip_relocation, catalina:       "3d77a4b086fa69e59b3f33b344131868588bcb558035b862961e514a0c3273e4"
+      sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae05feeb52801460e639d2440e3a523c4d5d0ca0626cc609cfb2dd1414fa5101"
+    end
+  
+    keg_only :versioned_formula
+  
+    deprecate! date: "2022-02-10", because: :unsupported
+  
+    depends_on "openjdk@17" # customized to address https://github.com/Homebrew/homebrew-core/issues/100260
+  
+    def cluster_name
+      "elasticsearch_#{ENV["USER"]}"
+    end
+  
+    def install
+      # Remove Windows files
+      rm_f Dir["bin/*.bat"]
+      rm_f Dir["bin/*.exe"]
+  
+      # Install everything else into package directory
+      libexec.install "bin", "config", "lib", "modules"
+  
+      inreplace libexec/"bin/elasticsearch-env",
+                "if [ -z \"$ES_PATH_CONF\" ]; then ES_PATH_CONF=\"$ES_HOME\"/config; fi",
+                "if [ -z \"$ES_PATH_CONF\" ]; then ES_PATH_CONF=\"#{etc}/elasticsearch\"; fi"
+  
+      # Set up Elasticsearch for local development:
+      inreplace "#{libexec}/config/elasticsearch.yml" do |s|
+        # 1. Give the cluster a unique name
+        s.gsub!(/#\s*cluster\.name: .*/, "cluster.name: #{cluster_name}")
+  
+        # 2. Configure paths
+        s.sub!(%r{#\s*path\.data: /path/to.+$}, "path.data: #{var}/lib/elasticsearch/")
+        s.sub!(%r{#\s*path\.logs: /path/to.+$}, "path.logs: #{var}/log/elasticsearch/")
+      end
+  
+      inreplace "#{libexec}/config/jvm.options" do |s|
+        s.gsub! "logs/gc.log", "#{var}/log/elasticsearch/gc.log"
+        s.gsub! "10-:-XX:UseAVX=2", "# 10-:-XX:UseAVX=2" if Hardware::CPU.arm?
+      end
+  
+      # Move config files into etc
+      (etc/"elasticsearch").install Dir[libexec/"config/*"]
+      (libexec/"config").rmtree
+  
+      bin.install libexec/"bin/elasticsearch",
+                  libexec/"bin/elasticsearch-keystore",
+                  libexec/"bin/elasticsearch-plugin",
+                  libexec/"bin/elasticsearch-translog"
+      bin.env_script_all_files(libexec/"bin", Language::Java.overridable_java_home_env)
+    end
+  
+    def post_install
+      # Make sure runtime directories exist
+      (var/"lib/elasticsearch").mkpath
+      (var/"log/elasticsearch").mkpath
+      ln_s etc/"elasticsearch", libexec/"config" unless (libexec/"config").exist?
+      (var/"elasticsearch/plugins").mkpath
+      ln_s var/"elasticsearch/plugins", libexec/"plugins" unless (libexec/"plugins").exist?
+      # fix test not being able to create keystore because of sandbox permissions
+      system bin/"elasticsearch-keystore", "create" unless (etc/"elasticsearch/elasticsearch.keystore").exist?
+    end
+  
+    def caveats
+      <<~EOS
+        Data:    #{var}/lib/elasticsearch/
+        Logs:    #{var}/log/elasticsearch/#{cluster_name}.log
+        Plugins: #{var}/elasticsearch/plugins/
+        Config:  #{etc}/elasticsearch/
+      EOS
+    end
+  
+    plist_options manual: "elasticsearch"
+  
+    def plist
+      <<~EOS
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+          <dict>
+            <key>KeepAlive</key>
+            <false/>
+            <key>Label</key>
+            <string>#{plist_name}</string>
+            <key>ProgramArguments</key>
+            <array>
+              <string>#{opt_bin}/elasticsearch</string>
+            </array>
+            <key>EnvironmentVariables</key>
+            <dict>
+            <!-- customize to address https://github.com/Homebrew/homebrew-core/issues/100260 -->
+            <key>JAVA_HOME</key>
+            <string>'/usr/libexec/java_home -v 17'</string>
+            </dict>
+            <key>RunAtLoad</key>
+            <true/>
+            <key>WorkingDirectory</key>
+            <string>#{var}</string>
+            <key>StandardErrorPath</key>
+            <string>#{var}/log/elasticsearch.log</string>
+            <key>StandardOutPath</key>
+            <string>#{var}/log/elasticsearch.log</string>
+          </dict>
+        </plist>
+      EOS
+    end
+  
+    test do
+      assert_includes(stable.url, "-oss-")
+  
+      port = free_port
+      system "#{bin}/elasticsearch-plugin", "list"
+      pid = testpath/"pid"
+      begin
+        system "#{bin}/elasticsearch", "-d", "-p", pid, "-Epath.data=#{testpath}/data", "-Ehttp.port=#{port}"
+        sleep 10
+        system "curl", "-XGET", "localhost:#{port}/"
+      ensure
+        Process.kill(9, pid.read.to_i)
+      end
+  
+      port = free_port
+      (testpath/"config/elasticsearch.yml").write <<~EOS
+        path.data: #{testpath}/data
+        path.logs: #{testpath}/logs
+        node.name: test-es-path-conf
+        http.port: #{port}
+      EOS
+  
+      cp etc/"elasticsearch/jvm.options", "config"
+      cp etc/"elasticsearch/log4j2.properties", "config"
+  
+      ENV["ES_PATH_CONF"] = testpath/"config"
+      pid = testpath/"pid"
+      begin
+        system "#{bin}/elasticsearch", "-d", "-p", pid
+        sleep 10
+        system "curl", "-XGET", "localhost:#{port}/"
+        output = shell_output("curl -s -XGET localhost:#{port}/_cat/nodes")
+        assert_match "test-es-path-conf", output
+      ensure
+        Process.kill(9, pid.read.to_i)
+      end
+    end
+  end
