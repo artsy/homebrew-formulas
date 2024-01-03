0e44af7a163b8fceb77162e6f4a1b6b3ac81a953
Hokusai CI <it@artsymail.com>
2024-01-03 18:38:25 +0000

    Release Hokusai Beta

--- Formula/hokusai-beta.rb
+++ Formula/hokusai-beta.rb
@@ -2,7 +2,7 @@ class HokusaiBeta < Formula
   desc 'Hokusai is a Docker + Kubernetes CLI for application developers'
   homepage 'https://github.com/artsy/hokusai'
   url 'https://artsy-provisioning-public.s3.amazonaws.com/hokusai/hokusai-beta-Darwin-x86_64.tar.gz'
-  sha256 'ac6d8c71d7685b6acff6d8206d3be7ca5546d8e6633f25eb67514a0dc413bcc5'
+  sha256 '665a79ebeeefdc152c91282aa87e1a5d0068f87bf0eb7a4995cba6883b7f52bf'
   version 'beta'
 
   def install
